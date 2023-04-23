f684caf1773f88f07ac9aec7ed15bed95e8b89b0
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-22 20:33:28 -0700

    imported svg code

--- index.js
+++ index.js
@@ -35,7 +35,18 @@ inquirer.prompt([
     }
 ])
 .then(({txt, color, shape, shapecolor}) => {
-   var data = "";
+   var data = `
+   <svg version="1.1"
+     width="300" height="200"
+     xmlns="http://www.w3.org/2000/svg">
+
+  <rect width="100%" height="100%" fill="red" />
+
+  <circle cx="150" cy="100" r="80" fill="green" />
+
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+</svg>`
     fs.writeFile("logo.svg", data, ()=> {
         console.log("Generated logo.svg");
     }
