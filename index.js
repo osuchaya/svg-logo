e58894bbe1a4d97dfbdc4eab0f89ce284b3c4656
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-22 21:13:53 -0700

    added render function to render logo

--- index.js
+++ index.js
@@ -50,14 +50,15 @@ inquirer.prompt([
    s.setColor(shapecolor)
    
    
-    var data = `
+    var data = 
+    `
    <svg version="1.1"
      width="300" height="200"
      xmlns="http://www.w3.org/2000/svg">
 
-  <rect width="100%" height="100%" fill="" />
+  ${s.render()}
 
-  <circle cx="150" cy="100" r="80" fill="${shapecolor}" />
+
 
   <text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${txt}</text>
 
